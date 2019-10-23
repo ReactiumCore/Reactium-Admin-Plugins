5cfd303d6aa74a2711f480ef5dcf47c5592f5cb8
John Dillick <john@dillick.us>
2019-10-22 20:55:05 -0400

    Fix middleware ordering.

--- src/app/server/middleware.js
+++ src/app/server/middleware.js
@@ -1,20 +1,25 @@
+const Enums = require('reactium-core/sdk/enums').default;
 const { routes, blueprints } = require('./middlewares/config');
 const { forceSSL } = require('./middlewares/forceSSL');
 
 module.exports = mw => {
-    return [
-        {
-            name: 'forceSSL',
-            use: forceSSL,
-        },
-        {
-            name: 'blueprints',
-            use: blueprints,
-        },
-        {
-            name: 'frontEndRoutes',
-            use: routes,
-        },
-        ...mw,
-    ];
+    mw.push({
+        name: 'forceSSL',
+        use: forceSSL,
+        order: Enums.priority.highest,
+    });
+
+    mw.push({
+        name: 'blueprints',
+        use: blueprints,
+        order: Enums.priority.highest,
+    });
+
+    mw.push({
+        name: 'frontEndRoutes',
+        use: routes,
+        order: Enums.priority.highest,
+    });
+
+    return mw;
 };
