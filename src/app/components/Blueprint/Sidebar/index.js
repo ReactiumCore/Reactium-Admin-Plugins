7ae336f2f6567ac6946b04b74a52b4a145a32b0e
Cam Tullos <cam@tullos.ninja>
2019-11-06 10:38:47 -0500

    WIP: Admin blueprint

---
+++ src/app/components/Blueprint/Sidebar/index.js
@@ -0,0 +1,87 @@
+import _ from 'underscore';
+import cn from 'classnames';
+import op from 'object-path';
+import PropTypes from 'prop-types';
+import React, {
+    forwardRef,
+    useImperativeHandle,
+    useEffect,
+    useLayoutEffect as useWindowEffect,
+    useRef,
+    useState,
+} from 'react';
+
+// Server-Side Render safe useLayoutEffect (useEffect when node)
+const useLayoutEffect =
+    typeof window !== 'undefined' ? useWindowEffect : useEffect;
+
+const ENUMS = {};
+
+/**
+ * -----------------------------------------------------------------------------
+ * Hook Component: Sidebar
+ * -----------------------------------------------------------------------------
+ */
+let Sidebar = ({ children, ...props }, ref) => {
+    // Refs
+    const containerRef = useRef();
+    const stateRef = useRef({
+        ...props,
+    });
+
+    // State
+    const [, setNewState] = useState(stateRef.current);
+
+    // Internal Interface
+    const setState = newState => {
+        // Update the stateRef
+        stateRef.current = {
+            ...stateRef.current,
+            ...newState,
+        };
+
+        // Trigger useEffect()
+        setNewState(stateRef.current);
+    };
+
+    const cx = () => {
+        const { className, namespace } = stateRef.current;
+        return cn({ [className]: !!className, [namespace]: !!namespace });
+    };
+
+    // Side Effects
+    useEffect(() => setState(props), Object.values(props));
+
+    // Renderer
+    const render = () => {
+        return (
+            <div ref={containerRef} className={cx()}>
+                {children}
+            </div>
+        );
+    };
+
+    // External Interface
+    useImperativeHandle(ref, () => ({
+        container: containerRef.current,
+        ref,
+        setState,
+        state: stateRef.current,
+    }));
+
+    // Render
+    return render();
+};
+
+Sidebar = forwardRef(Sidebar);
+
+Sidebar.ENUMS = ENUMS;
+
+Sidebar.propTypes = {
+    className: PropTypes.string,
+    namespace: PropTypes.string,
+};
+
+Sidebar.defaultProps = {};
+
+export { Sidebar as default };
