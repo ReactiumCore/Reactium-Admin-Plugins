a97da4be784cda6d51064863ca55c0ee1190ca8d
Cam Tullos <cam@tullos.ninja>
2019-11-06 19:02:49 -0500

    Cleaned up Blueprint component and added defaultBlueprint object to default state.

--- src/app/components/Blueprint/index.js
+++ src/app/components/Blueprint/index.js
@@ -4,24 +4,23 @@ import cn from 'classnames';
 import op from 'object-path';
 import Parse from 'appdir/api';
 import Reactium from 'reactium-core/sdk';
-import { useSelect, useStore } from 'reactium-core/easy-connect';
 import { Plugins } from 'reactium-core/components/Plugable';
+import { useSelect, useStore } from 'reactium-core/easy-connect';
 
 const ENUMS = {};
 
 const blueprintSelect = {
-    select: state => {
-        const { Blueprint, Router } = state;
-
-        const pathname = op.get(Router, 'match.path', '/');
-        const blueprintId = op.get(
-            Blueprint,
-            ['routesConfig', pathname, 'blueprint'],
+    select: state =>
+        op.get(
+            state,
+            [
+                'Blueprint',
+                'routesConfig',
+                op.get(state, 'Router.match.path', '/'),
+                'blueprint',
+            ],
             '',
-        );
-
-        return blueprintId;
-    },
+        ),
     shouldUpdate: ({ newState, prevState }) => newState !== prevState,
 };
 
@@ -33,53 +32,42 @@ const blueprintSelect = {
 const Blueprint = () => {
     const { getState } = useStore();
 
+    const { defaultBlueprint } = getState().Blueprint;
+
     const blueprintId = useSelect(blueprintSelect);
 
-    const blueprint = op.get(getState(), [
-        'Blueprint',
-        'blueprints',
-        blueprintId,
-    ]);
+    const blueprint = op.get(
+        getState(),
+        ['Blueprint', 'blueprints', blueprintId],
+        defaultBlueprint,
+    );
 
     const blueprintMeta = op.get(blueprint, 'meta', {});
+
+    const cname = (prefix, name) =>
+        cn(prefix, `${prefix}-${String(name).toLowerCase()}`);
+
     const sections = op.get(blueprint, 'sections', {});
+
+    const zoneMeta = value => op.get(value, 'meta', []);
 
     // Renderer
     const render = () => (
-        <main
-            className={cn(
-                'blueprint',
-                `blueprint-${blueprintId.toLowerCase()}`,
-            )}>
-            {Object.entries(sections).map(([name, value]) => {
-                const zones = op.get(value, 'zones', []);
-                const zoneMeta = op.get(value, 'meta', {});
-
-                return (
-                    <section
-                        key={name}
-                        className={cn(
-                            'section',
-                            `section-${name.toLowerCase()}`,
-                        )}>
-                        {zones.map(zone => (
-                            <div
-                                key={zone}
-                                className={cn(
-                                    'zone',
-                                    `zone-${zone.toLowerCase()}`,
-                                )}>
-                                <Plugins
-                                    zone={zone}
-                                    section={name}
-                                    {...blueprintMeta}
-                                    {...zoneMeta}
-                                />
-                            </div>
-                        ))}
-                    </section>
-                );
-            })}
+        <main className={cname('blueprint', blueprintId)}>
+            {Object.entries(sections).map(([name, value]) => (
+                <section key={name} className={cname('section', name)}>
+                    {op.get(value, 'zones', []).map(zone => (
+                        <div key={zone} className={cname('zone', zone)}>
+                            <Plugins
+                                zone={zone}
+                                section={name}
+                                {...blueprintMeta}
+                                {...zoneMeta(value)}
+                            />
+                        </div>
+                    ))}
+                </section>
+            ))}
         </main>
     );
 
--- src/app/components/Blueprint/state.js
+++ src/app/components/Blueprint/state.js
@@ -13,4 +13,20 @@ export default {
     // do not persist to local storage (default)
     // see https://www.npmjs.com/package/redux-local-persist
     persist: false,
+
+    defaultBlueprint: {
+        sections: {
+            sidebar: {
+                zones: ['admin-sidebar'],
+                meta: {},
+            },
+            main: {
+                zones: ['admin-header', 'admin-content', 'admin-actions'],
+                meta: {},
+            },
+        },
+        meta: {},
+        ID: 'Default',
+        description: 'Default blueprint',
+    },
 };
