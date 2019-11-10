d7eb53d87c1ce884994ea868acd1015a33a53b2d
Cam Tullos <cam@tullos.ninja>
2019-11-09 22:40:30 -0500

    Added menu item break bar and the Logout widget.

--- src/app/components/Admin/Logout/SidebarWidget/index.js
+++ src/app/components/Admin/Logout/SidebarWidget/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import MenuItem from 'components/Admin/MenuItem';
 
 export default () => (
-    <MenuItem label='Sign Out' route='/logout' icon='Linear.PowerSwitch' />
+    <>
+        <div className='menu-break' />
+        <MenuItem label='Sign Out' route='/logout' icon='Linear.PowerSwitch' />
+    </>
 );
