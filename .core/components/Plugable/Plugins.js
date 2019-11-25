05ac6930014f028bf11add4a8ebfaafcf0c45e57
John Dillick <john@dillick.us>
2019-11-25 15:38:48 -0500

    Core update. Excluding reactium-ui from plugin umd bundles.

--- .core/components/Plugable/Plugins.js
+++ .core/components/Plugable/Plugins.js
@@ -3,11 +3,11 @@
  * Imports
  * -----------------------------------------------------------------------------
  */
-import React, { Component, useContext } from 'react';
+import React, { useContext } from 'react';
 import Context from './Context';
 import op from 'object-path';
 import getComponents from 'dependencies/getComponents';
-import { useSelect } from 'reactium-core/easy-connect';
+import { useSelect } from 'reactium-core/sdk';
 import _ from 'underscore';
 
 const findComponent = (type, path, paths) => {
