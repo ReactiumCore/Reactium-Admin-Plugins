995ac987a528e112505a2c4797ab1fdefd70a956
Cam Tullos <cam@tullos.ninja>
2020-01-02 22:17:13 -0500

    Cleaned up Media Editor Breadcrumbs.

--- src/app/components/Admin/MediaEditor/Breadcrumbs/index.js
+++ src/app/components/Admin/MediaEditor/Breadcrumbs/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import _ from 'underscore';
 import op from 'object-path';
 import ENUMS from 'components/Admin/Media/enums';
+import { useHandle, useSelect } from 'reactium-core/sdk';
 import { Button, Icon } from '@atomic-reactor/reactium-ui';
-import Reactium, { __, useHandle, useSelect } from 'reactium-core/sdk';
 
 export default () => {
     const Editor = useHandle('MediaEditor');
@@ -14,22 +13,24 @@ export default () => {
 
     const visible = String(path).startsWith('/admin/media/edit/');
 
-    return !visible ? null : (
-        <ul className='ar-breadcrumbs'>
-            <li>
-                <Button
-                    appearance='pill'
-                    className='px-0'
-                    color='clear'
-                    size='sm'
-                    to='/admin/media/1'
-                    type='link'>
-                    <Icon name='Linear.Pictures' className='mr-xs-12' />
-                    {ENUMS.TEXT.MEDIA}
-                </Button>
-            </li>
-            <li className='uppercase'>{ENUMS.TEXT.EDIT}</li>
-            <li>{ID}</li>
-        </ul>
+    return (
+        visible && (
+            <ul className='ar-breadcrumbs'>
+                <li>
+                    <Button
+                        appearance='pill'
+                        className='px-0'
+                        color='clear'
+                        size='sm'
+                        to='/admin/media/1'
+                        type='link'>
+                        <Icon name='Linear.Pictures' className='mr-xs-12' />
+                        {ENUMS.TEXT.MEDIA}
+                    </Button>
+                </li>
+                <li className='uppercase'>{ENUMS.TEXT.EDIT}</li>
+                <li>{ID}</li>
+            </ul>
+        )
     );
 };
