41d32e7012864da435d60c323d6a394eba4ed1cc
Cam Tullos <cam@tullos.ninja>
2020-02-24 18:34:40 -0500

    cleaned up Content Editor stuff.

--- src/app/components/Admin/Content/reactium-hooks.js
+++ src/app/components/Admin/Content/reactium-hooks.js
@@ -1,3 +1,5 @@
+import ContentList from './List';
+import ContentEditor from './Editor';
 import Breadcrumbs from './Breadcrumbs';
 import Reactium from 'reactium-core/sdk';
 import HeaderWidget from './HeaderWidget';
@@ -16,6 +18,20 @@ Reactium.Plugin.register('AdminContent', Reactium.Enums.priority.lowest).then(
             component: HeaderWidget,
             order: 2,
             zone: ['admin-logo'],
+        });
+
+        Reactium.Zone.addComponent({
+            id: 'ADMIN-CONTENT-EDITOR',
+            component: ContentEditor,
+            order: Reactium.Enums.priority.lowest,
+            zone: ['admin-content-editor'],
+        });
+
+        Reactium.Zone.addComponent({
+            id: 'ADMIN-CONTENT-LIST',
+            component: ConentList,
+            order: Reactium.Enums.priority.lowest,
+            zone: ['admin-content-list'],
         });
     },
 );
