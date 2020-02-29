73b69c987d01a322cd49c392fe1afd3c783f7e57
Cam Tullos <cam@tullos.ninja>
2020-02-29 11:18:06 -0500

    Fixed RTE toolbar/sidebar z indices.

--- src/app/components/Admin/registered-components/RichTextEditor/FieldTypeRichText/Editor.js
+++ src/app/components/Admin/registered-components/RichTextEditor/FieldTypeRichText/Editor.js
@@ -48,6 +48,7 @@ export default ({ editor, ...props }) => {
                         placeholder={__(placeholder)}
                     />
                 </div>
+                <div className='line' />
             </section>
         );
     };
