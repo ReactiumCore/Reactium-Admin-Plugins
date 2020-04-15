092cb285b6c0b97c8e3b36d704a8ae75940437ca
Cam Tullos <cam@tullos.ninja>
2020-04-15 18:14:19 -0400

    @atomic-reactor/reactium-ui@0.1.117

--- src/app/components/Admin/Content/ActivityLog/ActivityChart.js
+++ src/app/components/Admin/Content/ActivityLog/ActivityChart.js
@@ -1,19 +1,63 @@
-import React from 'react';
+import _ from 'underscore';
+import moment from 'moment';
 import cn from 'classnames';
-import Reactium from 'reactium-core/sdk';
+import op from 'object-path';
+import React, { useEffect, useRef } from 'react';
+import Reactium, {
+    useDerivedState,
+    useFulfilledObject,
+    useHookComponent,
+} from 'reactium-core/sdk';
+
+const getData = log =>
+    log.reduce((obj, item) => {
+        const { createdAt } = item;
+        const date = moment(createdAt).format('L');
+        if (!op.has(obj, date)) op.set(obj, date, []);
+
+        op.set(item, 'date', date);
+        obj[date].push(item);
+
+        return obj;
+    }, {});
 
 const ActivityChart = props => {
     let { className, log = [], namespace } = props;
 
+    const containerRef = useRef();
+
+    //const { AreaChart } = useHookComponent('ReactiumUI');
+
+    const [state, setNewState] = useDerivedState({
+        data: undefined,
+    });
+
+    const [ready] = useFulfilledObject(state, ['data']);
+
     const cx = Reactium.Utils.cxFactory(namespace);
 
-    return <div className={cn(cx(), className)}>ActivityChart</div>;
+    const setState = newState => {
+        if (unMounted()) return;
+        setNewState(newState);
+    };
+
+    const unMounted = () => !containerRef.current;
+
+    useEffect(() => {
+        if (!state.data) setState({ data: getData(log) });
+    }, [state.data]);
+
+    return (
+        <div className={cn(cx(), className)} ref={containerRef}>
+            {ready && <div>HELLO</div>}
+        </div>
+    );
 };
 
 ActivityChart.defaultProps = {
     className: 'col-xs-12 col-md-8 col-lg-10',
     log: [],
-    namespace: 'activity-log-chart',
+    namespace: 'activity-chart',
 };
 
 export default ActivityChart;
