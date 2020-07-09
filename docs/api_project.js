c38ad1aaa2b91201d95b512f3ed1cce2a17aef73
Cam Tullos <cam@tullos.ninja>
2020-07-09 11:54:44 -0400

    Published 0.0.126

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-07-08T21:53:44.584Z",
+    "time": "2020-07-09T01:52:09.183Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
