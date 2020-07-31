6d4cd9e6220800d5f872fa308e5a511a6948eb3e
Cam Tullos <cam@tullos.ninja>
2020-07-31 13:34:12 -0400

    Clear cached capability keys on log out.

--- src/app/components/Admin/registered-components/CapabilityEditor/reactium-hooks.js
+++ src/app/components/Admin/registered-components/CapabilityEditor/reactium-hooks.js
@@ -2,3 +2,12 @@ import Reactium from 'reactium-core/sdk';
 import CapabilityEditor from './index';
 
 Reactium.Component.register('CapabilityEditor', CapabilityEditor);
+
+// Clear cached values
+Reactium.Hook.register('user.before.logout', () => {
+    const keys = Reactium.Cache.keys();
+    keys.forEach(key => {
+        if (!String(key).startsWith('capabilities')) return;
+        Reactium.Cache.del(key);
+    });
+});
