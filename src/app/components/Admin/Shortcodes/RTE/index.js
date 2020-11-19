9b9ae70bee0d479327a40316ab1ac101ea660160
Cam Tullos <cam@tullos.ninja>
2020-11-19 13:40:43 -0500

    Moved Shortcodes into Admin directory due to missing styles and refactored the Shortcodes RTE plugin registration.

--- src/app/components/Admin/Shortcodes/RTE/index.js
+++ src/app/components/Admin/Shortcodes/RTE/index.js
@@ -37,12 +37,11 @@ const FormatElement = ({ shortcode, ...props }) => {
     return <EditorComponent {...props} />;
 };
 
-const Plugin = Reactium.RTE.pluginFactory({
-    type: 'shortcode',
-    order: 100,
-});
+const PLUGIN = {
+    ID: 'shortcode',
+};
 
-Plugin.callback = editor => {
+export default editor => {
     const onButtonClick = e => {
         const btn = e.currentTarget;
         let { x, y, width } = btn.getBoundingClientRect();
@@ -51,19 +50,19 @@ Plugin.callback = editor => {
         y = Math.floor(window.innerHeight / 4);
 
         editor.panel
-            .setID(Plugin.type)
+            .setID(PLUGIN.ID)
             .setContent(<Panel selection={editor.selection} editor={editor} />)
             .moveTo(x, y)
             .show();
     };
 
     // register leaf format
-    Reactium.RTE.Format.register(Plugin.type, {
+    Reactium.RTE.Format.register(PLUGIN.ID, {
         element: FormatElement,
     });
 
     // register sidebar button
-    Reactium.RTE.Button.register(Plugin.type, {
+    Reactium.RTE.Button.register(PLUGIN.ID, {
         order: 62,
         sidebar: true,
         button: props => <SidebarButton {...props} onClick={onButtonClick} />,
@@ -73,9 +72,7 @@ Plugin.callback = editor => {
 
     const { isInline } = editor;
 
-    editor.isInline = n => (n.type === Plugin.type ? true : isInline(n));
+    editor.isInline = n => (n.type === PLUGIN.ID ? true : isInline(n));
 
     return editor;
 };
-
-export default Plugin;
