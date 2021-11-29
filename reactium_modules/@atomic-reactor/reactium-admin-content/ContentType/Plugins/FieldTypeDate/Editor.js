c371d850e4588c61c52b50978aec87657f30b3d1
Cam Tullos <>
2021-11-29 13:28:45 -1000

    Fixed date content type field initial value from saved content.

--- reactium_modules/@atomic-reactor/reactium-admin-content/ContentType/Plugins/FieldTypeDate/Editor.js
+++ reactium_modules/@atomic-reactor/reactium-admin-content/ContentType/Plugins/FieldTypeDate/Editor.js
@@ -1,37 +1,47 @@
 import _ from 'underscore';
+import moment from 'moment';
 import op from 'object-path';
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useHookComponent, useSyncState } from 'reactium-core/sdk';
+
+const formatDate = d => {
+    if (typeof d === 'string') {
+        d = new Date(d);
+    }
+    return d ? moment(d).format('L') : null;
+};
 
 const Editor = props => {
     const { editor, fieldName, options } = props;
 
     const state = useSyncState({
-        value: op.get(editor.value, fieldName),
+        value: formatDate(op.get(editor.value, fieldName)),
     });
 
     const { DatePicker } = useHookComponent('ReactiumUI');
 
     const ElementDialog = useHookComponent('ElementDialog');
 
-    const onSave = e => {
-        const { value } = state.get();
-        const formatted = value ? new Date(value) : null;
-        op.set(e.value, fieldName, formatted);
-    };
-
-    const onSelectDate = e => {
-        const selected = _.compact(e.selected || []);
-        const date = selected.length > 0 ? _.first(selected) : null;
-        state.set('value', date);
-    };
-
-    useEffect(() => {
+    const onLoad = useCallback(() => {
         editor.addEventListener('before-save', onSave);
         return () => {
             editor.removeEventListener('before-save', onSave);
         };
-    }, [editor]);
+    });
+
+    const onSave = useCallback(e => {
+        const { value } = state.get();
+        const formatted = value ? new Date(value) : null;
+        op.set(e.value, fieldName, formatted);
+    }, []);
+
+    const onSelectDate = useCallback(e => {
+        const selected = _.compact(e.selected || []);
+        const date = selected.length > 0 ? _.first(selected) : null;
+        state.set('value', date);
+    }, []);
+
+    useEffect(onLoad, [editor]);
 
     return (
         <ElementDialog {...props}>
