1200ff3c37e0987614aadfe442b932697a94edf5
Cam Tullos <cam@reactium.io>
2023-10-18 00:29:10 -1000

    no message

---
+++ src/app/components/Playground/reactium-route-playground.js
@@ -0,0 +1,10 @@
+import { Playground as component } from './Playground';
+
+export default [
+    {
+        id: 'route-Playground-1',
+        exact: true,
+        component,
+        path: ['/test'],
+    },
+];
